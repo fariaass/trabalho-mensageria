fc10a708c0e0f06e4bd33991bcc5d5fe50c9b19b
fariaass <farias.lucasteixeira@gmail.com>
2025-10-02 22:07:34 -0300

    feat: umidity controller

--- mensageria/controlador_umidade/main.go
+++ mensageria/controlador_umidade/main.go
@@ -1,20 +1,35 @@
-package controladorumidade
+package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"strconv"
 	"strings"
+	"syscall"
+	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-type Message struct {
-	
+type Sensor struct {
+	Id              string
+	Location        string
+	Name            string
+	AverageMoisture float64
 }
+
+type Message struct {
+	Sensors []Sensor
+}
+
+var (
+	moistureThreshold float64
+)
 
 func main() {
 	username := os.Getenv("RABBITMQ_USERNAME")
@@ -22,6 +37,13 @@ func main() {
 	host := os.Getenv("RABBITMQ_HOST")
 	port := os.Getenv("RABBITMQ_PORT")
 	queue := os.Getenv("RABBITMQ_QUEUE")
+
+	var err error
+	moistureThreshold, err = strconv.ParseFloat(os.Getenv("MOISTURE_THRESHOLD"), 64)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
 	conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%s/", username, password, host, port))
 	if err != nil {
 		log.Fatalf("failed to connect to rabbitmq: %v", err)
@@ -37,14 +59,24 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
+	if err := registerExchanges(ch); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if err := registerIrrigators(ch); err != nil {
+		log.Fatal(err.Error())
+	}
+
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, os.Kill)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
 main_loop:
 	for {
 		select {
 		case msg := <-msgsCh:
-			triggerIrrigators(msg)
+			if err := triggerIrrigators(ch, msg.Body); err != nil {
+				log.Printf("failed to trigger irrigators: %v", err)
+			}
 
 		case <-c:
 			fmt.Println("interrupting...")
@@ -84,7 +116,7 @@ func registerConsumer(ch *amqp.Channel, queue string) (<-chan amqp.Delivery, err
 	return msgs, nil
 }
 
-func registerExchanges(ch *amqp.Channel, queue string) (<-chan amqp.Delivery, error) {
+func registerExchanges(ch *amqp.Channel) error {
 	if err := ch.ExchangeDeclare(
 		"all",
 		amqp.ExchangeFanout,
@@ -94,33 +126,150 @@ func registerExchanges(ch *amqp.Channel, queue string) (<-chan amqp.Delivery, er
 		false,
 		nil,
 	); err != nil {
-		return nil, fmt.Errorf("failed to declare \"all\" exchange: %w", err)
+		return fmt.Errorf("failed to declare exchange \"all\": %w", err)
 	}
 
-	for i := range 4 {
-		quadrant := fmt.Sprintf("q%d", i+1)
-		if err := ch.ExchangeDeclare(
-			quadrant,
-			amqp.ExchangeTopic,
+	if err := ch.ExchangeDeclare(
+		"quadrants",
+		amqp.ExchangeTopic,
+		false,
+		false,
+		false,
+		false,
+		nil,
+	); err != nil {
+		return fmt.Errorf("failed to declare exchange \"quadrants\": %w", err)
+	}
+
+	return nil
+}
+
+func registerIrrigators(ch *amqp.Channel) error {
+	for i := range strings.SplitSeq(os.Getenv("IRRIGATORS_LIST"), ",") {
+		queue, err := ch.QueueDeclare(
+			i,
 			false,
 			false,
 			false,
 			false,
 			nil,
-		); err != nil {
-			return nil, fmt.Errorf("failed to declare \"%s\" exchange: %w", quadrant, err)
+		)
+		if err != nil {
+			return fmt.Errorf("failed to declare queue \"%s\": %w", i, err)
+		}
+
+		err = ch.ExchangeDeclare(
+			i,
+			amqp.ExchangeDirect,
+			false,
+			false,
+			false,
+			false,
+			nil,
+		)
+		if err != nil {
+			return fmt.Errorf("failed to declare exchange \"%s\": %w", i, err)
+		}
+
+		irrigatorFields := strings.Split(i, "-")
+		if len(irrigatorFields) != 3 {
+			return fmt.Errorf("failed to parse irrigator fields: %s", irrigatorFields)
+		}
+
+		ch.QueueBind(
+			queue.Name,
+			"",
+			"all",
+			false,
+			nil,
+		)
+
+		ch.QueueBind(
+			queue.Name,
+			irrigatorFields[1],
+			"quadrants",
+			false,
+			nil,
+		)
+
+		ch.QueueBind(
+			queue.Name,
+			i,
+			i,
+			false,
+			nil,
+		)
+	}
+
+	return nil
+}
+
+func triggerIrrigators(ch *amqp.Channel, data []byte) error {
+	var msg Message
+	if err := json.Unmarshal(data, &msg); err != nil {
+		return fmt.Errorf("failed to unmarshal message content: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	count := 0
+	sensorsUnderThreshold := map[string][]string{}
+	payload := amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte("irrigate"),
+	}
+
+	for _, sensor := range msg.Sensors {
+		if sensor.AverageMoisture <= moistureThreshold {
+			sensorsUnderThreshold[sensor.Location] = append(sensorsUnderThreshold[sensor.Location], sensor.Id)
+			count++
 		}
 	}
 
-	return msgs, nil
-}
+	if count == len(msg.Sensors) {
+		if err := ch.PublishWithContext(
+			ctx,
+			"all",
+			"",
+			false,
+			false,
+			payload,
+		); err != nil {
+			return fmt.Errorf("failed to publish message in exchange \"all\": %w", err)
+		}
 
-func triggerIrrigators(data []byte) {
-	var msg Message
-	if err := json.Unmarshal(data, &msg); err != nil {
-		log.Printf("failed to unmarshal message content: %v", err)
-		return
+		return nil
 	}
 
+	errs := []error{}
+	for k, v := range sensorsUnderThreshold {
+		if len(v) == 1 {
+			if err := ch.PublishWithContext(
+				ctx,
+				v[0],
+				v[0],
+				false,
+				false,
+				payload,
+			); err != nil {
+				errs = append(errs, fmt.Errorf("failed to publish message in exchange \"%s\": %w", v[0], err))
+			}
 
+			continue
+		}
+		
+		if err := ch.PublishWithContext(
+			ctx,
+			"quadrants",
+			k,
+			false,
+			false,
+			payload,
+		); err != nil {
+			errs = append(errs, fmt.Errorf("failed to publish message in exchange \"%s\": %w", k, err))
+		}
+	}
+
+	return errors.Join(errs...)
 }
